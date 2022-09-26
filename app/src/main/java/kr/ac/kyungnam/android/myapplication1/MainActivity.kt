068e5e2a6cde5cbb855386131f45e106c1c4552e
dongmin Lee <ldm030475@naver.com>
2022-09-26 13:35:42 +0900

    screenShot

--- app/src/main/java/kr/ac/kyungnam/android/myapplication1/MainActivity.kt
+++ app/src/main/java/kr/ac/kyungnam/android/myapplication1/MainActivity.kt
@@ -12,7 +12,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        button1=findViewById<Button>(R.id.button)
+        button1=findViewById<Button>(R.id.button1)
 
         button1.setOnClickListener {
             Toast.makeText(applicationContext,"버튼을 눌렀어요ggggg", Toast.LENGTH_SHORT).show()
